7704461406bed8f782c6a3030cf1c3dd67b8db86
pkw1997122 <1369380646@qq.com>
2019-08-08 16:48:01 +0800

    我的第一个提交

--- MyTestProject/src/main/com/gkzx/Test.java
+++ MyTestProject/src/main/com/gkzx/Test.java
@@ -2,7 +2,6 @@ package main.com.gkzx;
 
 public class Test {
     public static void main(String arg[]){
-        System.out.println("new soul");
         System.out.println("i am a new soul");
     }
 }
