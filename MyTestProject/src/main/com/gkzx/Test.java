1bb97e3b661443c4fa6f12a957d032c3d5ad01c6
邹翰文 <53244346+pkw1997122@users.noreply.github.com>
2019-08-08 23:17:07 +0800

    特性：提交测试2

--- MyTestProject/src/main/com/gkzx/Test.java
+++ MyTestProject/src/main/com/gkzx/Test.java
@@ -2,6 +2,8 @@ package main.com.gkzx;
 
 public class Test {
     public static void main(String arg[]){
+        System.out.println("emily bloom");
+        System.out.println("playboy");
         System.out.println("new soul");
     }
 }
