868606ed0f7100a3f54bb7aa8b58eb033f465617
pkw1997122 <1369380646@qq.com>
2019-08-08 14:25:18 +0800

    特性：建个项目

---
+++ MyTestProject/src/main/com/gkzx/Test.java
@@ -0,0 +1,7 @@
+package main.com.gkzx;
+
+public class Test {
+    public static void main(String arg[]){
+        System.out.println("new soul");
+    }
+}
---
+++ demoprotest/src/java/DemoTest01.java
@@ -0,0 +1,7 @@
+package java;
+
+public class DemoTest01 {
+    public static void main(String arg[]){
+
+    }
+}
