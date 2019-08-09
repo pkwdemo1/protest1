1de24b9f0713e49eb03a01a8f50c955f22a666b4
pkw1997122 <1369380646@qq.com>
2019-08-09 17:08:12 +0800

    特性：测试信息1

--- MyTestProject/src/main/com/gkzx/Test.java
+++ MyTestProject/src/main/com/gkzx/Test.java
@@ -2,7 +2,6 @@ package main.com.gkzx;
 
 public class Test {
     public static void main(String arg[]){
-        System.out.println("emily bloom");
         System.out.println("航天城十三太保");
         System.out.println("you said that you love the rain");
     }
