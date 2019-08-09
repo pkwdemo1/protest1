04df6be443993856453d0d7d21870da653cf8a7e
pkw1997122 <1369380646@qq.com>
2019-08-09 09:41:06 +0800

    特性：测试信息1

--- MyTestProject/src/main/com/gkzx/Test.java
+++ MyTestProject/src/main/com/gkzx/Test.java
@@ -3,7 +3,7 @@ package main.com.gkzx;
 public class Test {
     public static void main(String arg[]){
         System.out.println("emily bloom");
-        System.out.println("playboy");
+        System.out.println("航天城十三太保");
         System.out.println("new soul");
     }
 }
