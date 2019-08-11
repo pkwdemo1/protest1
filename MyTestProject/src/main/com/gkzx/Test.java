d49f20878dda92880b63851d4b757a52739001cd
邹翰文 <53244346+pkw1997122@users.noreply.github.com>
2019-08-11 21:46:02 +0800

    特性：测试代码 (#8)

--- MyTestProject/src/main/com/gkzx/Test.java
+++ MyTestProject/src/main/com/gkzx/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String arg[]){
         System.out.println("航天城十三太保");
         System.out.println("you said that you love the rain");
+        System.out.println("詹姆斯哈登");
     }
 }
